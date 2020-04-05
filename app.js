648158827f09c2ed4e1977ed5f6b5d3235cab1e2
007atulpandey <46052653+007atulpandey@users.noreply.github.com>
2020-04-05 13:30:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,32 @@ const text = document.querySelector('.text');
 const btn = document.querySelector('.submit');
 
 
+
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
 recognition.onstart= function(){
-    console.log("text is activ");
+    console.log("text is active");
 }
 
 recognition.onresult=function(event){
-    const cuurent=event.resultIndex;
+    const current=event.resultIndex;
     const transcript= event.results[current][0].transcript;
     text.textContent=transcript;
+    readOutLoud(transcript);
 }
-
+// recognition.start();
 btn.addEventListener('click',()=>{
     recognition.start();
 
 });
+
+// readOutLoud(document.querySelector('.welcome').textContent);
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text= message;
+    speech.volume=1;
+    speech.rate=1;
+    speech.pitch=1;
+    window.speechSynthesis.speak(speech);
+}
